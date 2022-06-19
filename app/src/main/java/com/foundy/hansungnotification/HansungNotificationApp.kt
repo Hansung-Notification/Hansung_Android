fd1db7899c5d667782814236d5f201b931d238a3
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-06-19 21:10:45 +0900

    Refactor to use Clean architecture

---
+++ app/src/main/java/com/foundy/hansungnotification/HansungNotificationApp.kt
@@ -0,0 +1,7 @@
+package com.foundy.hansungnotification
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HansungNotificationApp : Application()
--- presentation/src/main/java/com/foundy/presentation/MainActivity.kt
+++ presentation/src/main/java/com/foundy/presentation/MainActivity.kt
@@ -1,19 +1,19 @@
-package com.foundy.hansungnotification
+package com.foundy.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.foundy.hansungnotification.databinding.ActivityMainBinding
+import com.foundy.presentation.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
-    private val binding :ActivityMainBinding get() = requireNotNull(_binding)
+    private val binding: ActivityMainBinding get() = requireNotNull(_binding)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
     }
 }
