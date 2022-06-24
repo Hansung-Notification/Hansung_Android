818835f1695907cff874164b1be75f5a8956f581
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-06-24 21:36:08 +0900

    Fix dark mode UI

--- presentation/src/main/java/com/foundy/presentation/view/webview/WebViewActivity.kt
+++ presentation/src/main/java/com/foundy/presentation/view/webview/WebViewActivity.kt
@@ -2,9 +2,14 @@ package com.foundy.presentation.view.webview
 
 import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebSettings.FORCE_DARK_OFF
+import android.webkit.WebSettings.FORCE_DARK_ON
 import android.webkit.WebViewClient
+import androidx.webkit.WebSettingsCompat
+import androidx.webkit.WebViewFeature
 import com.foundy.domain.model.Notice
 import com.foundy.presentation.databinding.ActivityWebViewBinding
 import com.google.gson.Gson
@@ -34,5 +39,20 @@ class WebViewActivity : AppCompatActivity() {
         binding.webView.apply {
             webViewClient = WebViewClient()
         }.loadUrl(notice.url)
+
+        setWebViewThemeMode()
+    }
+
+    private fun setWebViewThemeMode() {
+        if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
+            when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                Configuration.UI_MODE_NIGHT_YES -> {
+                    WebSettingsCompat.setForceDark(binding.webView.settings, FORCE_DARK_ON)
+                }
+                Configuration.UI_MODE_NIGHT_NO, Configuration.UI_MODE_NIGHT_UNDEFINED -> {
+                    WebSettingsCompat.setForceDark(binding.webView.settings, FORCE_DARK_OFF)
+                }
+            }
+        }
     }
 }
