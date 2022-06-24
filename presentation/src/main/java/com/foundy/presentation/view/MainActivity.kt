e3a68b3e80597e26e6a02af58fa9cb7bb1019388
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-06-25 00:09:37 +0900

    Add toolBar in main view

--- presentation/src/main/java/com/foundy/presentation/view/MainActivity.kt
+++ presentation/src/main/java/com/foundy/presentation/view/MainActivity.kt
@@ -3,6 +3,7 @@ package com.foundy.presentation.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.foundy.presentation.R
 import com.foundy.presentation.databinding.ActivityMainBinding
@@ -19,12 +20,13 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initBottomNavigationView()
-    }
-
-    private fun initBottomNavigationView() {
         val nhf = supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
         val navController = nhf.navController
+        val appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(R.id.noticeFragment, R.id.favoriteFragment),
+            fallbackOnNavigateUpListener = ::onSupportNavigateUp
+        )
+        binding.toolBar.setupWithNavController(navController, appBarConfiguration)
         binding.bottomNav.setupWithNavController(navController)
     }
 }
