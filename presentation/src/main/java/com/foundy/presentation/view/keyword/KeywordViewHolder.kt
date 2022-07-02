fc36d7ab767f74ad85c006b477ceda0c1a5fe645
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-07-02 22:01:08 +0900

    Implement messaging, keyword DB system

--- presentation/src/main/java/com/foundy/presentation/view/keyword/KeywordViewHolder.kt
+++ presentation/src/main/java/com/foundy/presentation/view/keyword/KeywordViewHolder.kt
@@ -1,19 +1,33 @@
 package com.foundy.presentation.view.keyword
 
+import androidx.lifecycle.LifecycleCoroutineScope
+import androidx.lifecycle.findViewTreeLifecycleOwner
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
+import com.foundy.domain.model.Keyword
 import com.foundy.presentation.databinding.ItemKeywordBinding
-import com.foundy.presentation.model.KeywordUiState
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class KeywordViewHolder(
     private val binding: ItemKeywordBinding,
+    private val lifecycleScope: LifecycleCoroutineScope? = null,
+    private val onClickDelete: (keyword: String) -> Unit
 ) : RecyclerView.ViewHolder(binding.root) {
 
-    fun bind(keywordUiState: KeywordUiState) {
+    fun bind(keyword: Keyword) {
         binding.apply {
-            title.text = keywordUiState.keyword
+            title.text = keyword.title
 
             deleteButton.setOnClickListener {
-                keywordUiState.onClickDelete()
+                // 빠르게 여러번 누르는 것을 방지한다.
+                val scope = lifecycleScope ?: it.findViewTreeLifecycleOwner()?.lifecycleScope
+                scope?.launch {
+                    deleteButton.isEnabled = false
+                    delay(500)
+                    deleteButton.isEnabled = true
+                }
+                onClickDelete(keyword.title)
             }
         }
     }
--- presentation/src/main/java/com/foundy/presentation/view/keyword/KeywordViewModel.kt
+++ presentation/src/main/java/com/foundy/presentation/view/keyword/KeywordViewModel.kt
@@ -5,7 +5,12 @@ import com.foundy.domain.model.Keyword
 import com.foundy.domain.usecase.keyword.AddKeywordUseCase
 import com.foundy.domain.usecase.keyword.ReadKeywordListUseCase
 import com.foundy.domain.usecase.keyword.RemoveKeywordUseCase
-import com.foundy.presentation.model.KeywordUiState
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ServerValue
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.ktx.messaging
+import com.github.kimcore.inko.Inko.Companion.asEnglish
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -17,14 +22,10 @@ class KeywordViewModel @Inject constructor(
     private val removeKeywordUseCase: RemoveKeywordUseCase
 ) : ViewModel() {
 
-    val keywordList = readKeywordListUseCase().asLiveData().map { it.map(::createKeywordUiState) }
+    val keywordList = readKeywordListUseCase().asLiveData()
 
-    private fun createKeywordUiState(keyword: Keyword): KeywordUiState {
-        return KeywordUiState(
-            keyword = keyword.title,
-            onClickDelete = { removeKeywordItem(keyword) }
-        )
-    }
+    private val keywordsReference: DatabaseReference
+        get() = Firebase.database.reference.child("keywords")
 
     fun addKeywordItem(keyword: Keyword) {
         viewModelScope.launch {
@@ -32,13 +33,45 @@ class KeywordViewModel @Inject constructor(
         }
     }
 
-    private fun removeKeywordItem(keyword: Keyword) {
+    fun removeKeywordItem(keyword: Keyword) {
         viewModelScope.launch {
             removeKeywordUseCase(keyword)
         }
     }
 
     fun hasKeyword(keyword: String): Boolean {
-        return keywordList.value?.firstOrNull { it.keyword == keyword } != null
+        return keywordList.value?.firstOrNull { it.title == keyword } != null
+    }
+
+    fun subscribeTo(topic: String, onFailure: (Exception) -> Unit) {
+        // 한글로된 토픽을 못쓰기 때문에 변환을 해준다.
+        Firebase.messaging.subscribeToTopic(topic.asEnglish).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                keywordsReference.child(topic).setValue(ServerValue.increment(1))
+                    .addOnCompleteListener { dbTask ->
+                        if (!dbTask.isSuccessful) {
+                            onFailure(dbTask.exception!!)
+                        }
+                    }
+            } else if (!task.isSuccessful) {
+                onFailure(task.exception!!)
+            }
+        }
+    }
+
+    fun unsubscribeFrom(topic: String, onFailure: (Exception) -> Unit) {
+        // 한글로된 토픽을 못쓰기 때문에 변환을 해준다.
+        Firebase.messaging.unsubscribeFromTopic(topic.asEnglish).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                keywordsReference.child(topic).setValue(ServerValue.increment(-1))
+                    .addOnCompleteListener { dbTask ->
+                        if (!dbTask.isSuccessful) {
+                            onFailure(dbTask.exception!!)
+                        }
+                    }
+            } else if (!task.isSuccessful) {
+                onFailure(task.exception!!)
+            }
+        }
     }
 }
