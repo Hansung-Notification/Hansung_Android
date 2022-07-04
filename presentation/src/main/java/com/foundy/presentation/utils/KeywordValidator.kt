aef64a5bb4bca31ff34bdb30475698952e7c6e73
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-07-04 13:48:11 +0900

    Improve warning message

--- presentation/src/main/java/com/foundy/presentation/utils/KeywordValidator.kt
+++ presentation/src/main/java/com/foundy/presentation/utils/KeywordValidator.kt
@@ -12,7 +12,7 @@ object KeywordValidator {
     open class KeywordInvalidException(message: String) : Exception(message)
     class MinLengthException : KeywordInvalidException("두 글자 이상 입력해주세요.")
     class AlreadyExistsException : KeywordInvalidException("이미 존재하는 키워드입니다.")
-    class InvalidCharacterException : KeywordInvalidException("한글과 숫자만 입력할 수 있어요.")
+    class InvalidCharacterException : KeywordInvalidException("완성된 한글과 숫자만 입력할 수 있어요.")
 
     operator fun invoke(keyword: String, registeredKeywordList: List<Keyword>): Boolean {
         if (!PATTERN.matcher(keyword).matches()) throw InvalidCharacterException()
