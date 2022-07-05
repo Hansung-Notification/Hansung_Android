a3c63a04fb11f39ba3cd5f971aea6cfae1230420
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-07-05 22:43:43 +0900

    Implement recent query feature

--- presentation/src/main/java/com/foundy/presentation/view/search/SearchActivity.kt
+++ presentation/src/main/java/com/foundy/presentation/view/search/SearchActivity.kt
@@ -11,14 +11,19 @@ import com.foundy.presentation.databinding.ActivitySearchBinding
 import com.foundy.presentation.view.MainViewModel
 import com.foundy.presentation.view.common.PagingLoadStateAdapter
 import com.foundy.presentation.view.notice.NoticeAdapter
+import com.paulrybitskyi.persistentsearchview.adapters.model.SuggestionItem
+import com.paulrybitskyi.persistentsearchview.listeners.OnSuggestionChangeListener
+import com.paulrybitskyi.persistentsearchview.utils.SuggestionCreationUtil
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SearchActivity : AppCompatActivity() {
 
-    private val viewModel: MainViewModel by viewModels()
+    private val mainViewModel: MainViewModel by viewModels()
+    private val searchViewModel: SearchViewModel by viewModels()
 
     private var _binding: ActivitySearchBinding? = null
     private val binding: ActivitySearchBinding get() = requireNotNull(_binding)
@@ -41,25 +46,50 @@ class SearchActivity : AppCompatActivity() {
 
     private fun initSearchView(adapter: NoticeAdapter) = with(binding.searchView) {
         expand(true)
+
+        searchViewModel.recentQueries.observe(this@SearchActivity) {
+            val recentList = SuggestionCreationUtil.asRecentSearchSuggestions(it)
+            setSuggestions(recentList, false)
+        }
         setOnSearchConfirmedListener { searchView, query ->
             searchView.collapse()
-            lifecycleScope.launch {
-                viewModel.search(query ?: "").collectLatest {
-                    adapter.submitData(lifecycle, it)
+            searchNotices(query, adapter)
+            searchViewModel.addOrUpdateRecent(query)
+        }
+        setOnSuggestionChangeListener(object : OnSuggestionChangeListener {
+            override fun onSuggestionPicked(suggestion: SuggestionItem?) {
+                val query = suggestion?.itemModel?.text
+                query?.let {
+                    searchNotices(it, adapter)
+                    lifecycleScope.launch {
+                        // 검색 항목을 누르자마자 순서가 변경되어 보기 안좋기 때문에 딜레이를 준다.
+                        delay(400)
+                        searchViewModel.addOrUpdateRecent(it)
+                    }
                 }
             }
-        }
+
+            override fun onSuggestionRemoved(suggestion: SuggestionItem?) {
+                suggestion?.itemModel?.text?.let { searchViewModel.removeRecent(it) }
+            }
+        })
         setOnLeftBtnClickListener {
             finish()
         }
     }
 
-    private fun initRecyclerView(adapter: NoticeAdapter) {
-        binding.recyclerView.apply {
-            this.adapter = adapter.withLoadStateFooter(
-                PagingLoadStateAdapter { adapter.retry() }
-            )
-            layoutManager = LinearLayoutManager(context)
+    private fun initRecyclerView(adapter: NoticeAdapter) = with(binding.recyclerView) {
+        this.adapter = adapter.withLoadStateFooter(
+            PagingLoadStateAdapter { adapter.retry() }
+        )
+        layoutManager = LinearLayoutManager(context)
+    }
+
+    private fun searchNotices(query: String, adapter: NoticeAdapter) {
+        lifecycleScope.launch {
+            mainViewModel.searchNotices(query).collectLatest {
+                adapter.submitData(lifecycle, it)
+            }
         }
     }
 }
