a14292689322e5294765757f3a23aa96dbe34be0
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-07-06 19:16:26 +0900

    Fix tests

--- app/src/androidTest/java/com/foundy/hansungnotification/SearchViewModelTest.kt
+++ app/src/androidTest/java/com/foundy/hansungnotification/SearchViewModelTest.kt
@@ -1,24 +1,23 @@
 package com.foundy.hansungnotification
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.foundy.domain.usecase.query.AddRecentQueryUseCase
 import com.foundy.domain.usecase.query.GetRecentQueryListUseCase
 import com.foundy.domain.usecase.query.RemoveRecentQueryUseCase
 import com.foundy.domain.usecase.query.UpdateRecentQueryUseCase
 import com.foundy.hansungnotification.fake.FakeQueryRepositoryImpl
-import com.foundy.hansungnotification.utils.getOrAwaitValue
+import com.foundy.hansungnotification.utils.observeForTesting
 import com.foundy.presentation.view.search.SearchViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
 @OptIn(ExperimentalCoroutinesApi::class)
-@RunWith(AndroidJUnit4::class)
 class SearchViewModelTest {
 
     private val testDispatcher = UnconfinedTestDispatcher()
@@ -42,15 +41,20 @@ class SearchViewModelTest {
         val query1 = "장학금"
         val query2 = "학생"
 
-        // 구독을 해야지 값이 변경된다.
-        searchViewModel.recentQueries.observeForever {}
-        searchViewModel.addOrUpdateRecent(query1)
-        assertEquals(1, searchViewModel.recentQueries.getOrAwaitValue().size)
+        searchViewModel.recentQueries.observeForTesting {
+            runBlocking {
+                searchViewModel.addOrUpdateRecent(query1)
+                delay(1)
+                assertEquals(1, searchViewModel.recentQueries.value?.size)
 
-        searchViewModel.addOrUpdateRecent(query1)
-        assertEquals(1, searchViewModel.recentQueries.getOrAwaitValue().size)
+                searchViewModel.addOrUpdateRecent(query1)
+                delay(1)
+                assertEquals(1, searchViewModel.recentQueries.value?.size)
 
-        searchViewModel.addOrUpdateRecent(query2)
-        assertEquals(1, searchViewModel.recentQueries.getOrAwaitValue().size)
+                searchViewModel.addOrUpdateRecent(query2)
+                delay(1)
+                assertEquals(2, searchViewModel.recentQueries.value?.size)
+            }
+        }
     }
 }
