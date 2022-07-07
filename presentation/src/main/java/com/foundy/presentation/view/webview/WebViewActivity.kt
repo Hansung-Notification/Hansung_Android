9fad7ff63888baa33cf36ac9758ce6693502d962
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-07-07 23:37:23 +0900

    Add comments in WebViewActivity.kt

--- presentation/src/main/java/com/foundy/presentation/view/webview/WebViewActivity.kt
+++ presentation/src/main/java/com/foundy/presentation/view/webview/WebViewActivity.kt
@@ -28,18 +28,34 @@ class WebViewActivity : AppCompatActivity() {
     private val binding: ActivityWebViewBinding get() = requireNotNull(_binding)
 
     companion object {
-
-        private const val IMAGE_CSS = "img{display: inline; height: auto; max-width: 100%;}"
-        private const val IMAGE_SETTING_SCRIPT = """
-            var styles = `${IMAGE_CSS}`;
+        /**
+         * 웹 페이지의 이미지의 너비가 너무 커서 스크린 밖으로 벗어나지 않도록 하는 스크립트이다.
+         *
+         * 공식 페이지(?layout=unknown)의 경우 해당 문제가 없으나 이 앱에서 이용하는 url의 경우는 문제가 있다.
+         */
+        private const val IMAGE_WIDTH_SCRIPT = """
+            var styles = `img{display: inline; height: auto; max-width: 100%;}`;
             var styleSheet = document.createElement("style");
             styleSheet.innerText = styles;
             document.head.appendChild(styleSheet);
         """
+
+        /**
+         * 페이지의 줌을 최대 3배까지 가능하게 하는 스크립트이다.
+         *
+         * 기존에는 1배까지로 줌을 지원하지 않는다. viewport에 접근하여 수정하고 있다.
+         */
         private const val ZOOM_SETTING_SCRIPT = """
             var content = `initial-scale=1, minimum-scale=0.5, maximum-scale=3.0, user-scalable=yes`;
             document.getElementsByName('viewport')[0].setAttribute('content', content);
         """
+
+        /**
+         * 테이블의 너비를 고치는 스크립트이다.
+         *
+         * 한성대 공지 페이지에서 테이블의 너비가 고정되어 잘리는 문제를 해결한다. 모든 table과 td들의 너비를 무효화하고
+         * `auto` 값을 가진 스타일 시트를 헤더에 추가한다.
+         */
         private const val TABLE_WIDTH_SCRIPT = """
             const tables = document.getElementsByTagName("table");
             const tableDatas = document.getElementsByTagName("td");
@@ -97,7 +113,7 @@ class WebViewActivity : AppCompatActivity() {
                 override fun onPageFinished(view: WebView?, url: String?) {
                     try {
                         evaluateJavascript(
-                            IMAGE_SETTING_SCRIPT +
+                            IMAGE_WIDTH_SCRIPT +
                                     ZOOM_SETTING_SCRIPT +
                                     TABLE_WIDTH_SCRIPT
                         ) {}
