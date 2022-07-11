fd9a90964a72dca9111875999f850209dfc94985
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-07-12 00:49:48 +0900

    Enable persistence of firebase DB

--- app/src/main/java/com/foundy/hansungnotification/HansungNotificationApp.kt
+++ app/src/main/java/com/foundy/hansungnotification/HansungNotificationApp.kt
@@ -1,7 +1,15 @@
 package com.foundy.hansungnotification
 
 import android.app.Application
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class HansungNotificationApp : Application()
+class HansungNotificationApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Firebase.database.setPersistenceEnabled(true)
+    }
+}
