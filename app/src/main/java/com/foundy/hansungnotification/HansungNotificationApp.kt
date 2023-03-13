89bd2f66cf80a278e7d6657d7aad268f5dca46f6
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-03-13 21:06:18 +0900

    알림 채널 앱 실행시 생성하도록 수정

--- app/src/main/java/com/foundy/hansungnotification/HansungNotificationApp.kt
+++ app/src/main/java/com/foundy/hansungnotification/HansungNotificationApp.kt
@@ -1,8 +1,11 @@
 package com.foundy.hansungnotification
 
-import android.Manifest
 import android.app.Application
-import com.eazypermissions.coroutinespermission.PermissionManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import com.foundy.presentation.R
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.HiltAndroidApp
@@ -13,5 +16,16 @@ class HansungNotificationApp : Application() {
     override fun onCreate() {
         super.onCreate()
         Firebase.database.setPersistenceEnabled(true)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val channel = NotificationChannel(
+                getString(R.string.firebase_keyword_noti_channel_id),
+                getString(R.string.keyword_notification),
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
--- presentation/src/main/java/com/foundy/presentation/service/HansungFirebaseMessagingService.kt
+++ presentation/src/main/java/com/foundy/presentation/service/HansungFirebaseMessagingService.kt
@@ -1,12 +1,10 @@
 package com.foundy.presentation.service
 
-import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
-import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.foundy.domain.model.Notice
@@ -102,15 +100,6 @@ class HansungFirebaseMessagingService : FirebaseMessagingService() {
 
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                channelId,
-                getString(R.string.keyword_notification),
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-            notificationManager.createNotificationChannel(channel)
-        }
 
         notificationManager.notify(id, notificationBuilder.build())
     }
