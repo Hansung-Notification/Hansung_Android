50a50c8cbde168ee7a74557bffa83be6ff959b78
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-03-13 20:47:30 +0900

    Request post notification permission

--- app/src/main/java/com/foundy/hansungnotification/HansungNotificationApp.kt
+++ app/src/main/java/com/foundy/hansungnotification/HansungNotificationApp.kt
@@ -1,6 +1,8 @@
 package com.foundy.hansungnotification
 
+import android.Manifest
 import android.app.Application
+import com.eazypermissions.coroutinespermission.PermissionManager
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.HiltAndroidApp
